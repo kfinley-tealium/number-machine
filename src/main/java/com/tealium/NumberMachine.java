3c3a04fc5f8905c51df1344064d75da73f093d07
Kirk Finley <kirk.finley@tealium.com>
2019-02-19 09:25:47 -0800

    Initial add

---
+++ src/main/java/com/tealium/NumberMachine.java
@@ -0,0 +1,28 @@
+package com.tealium;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class NumberMachine {
+
+	public List<Integer> getFactors(int number) {
+
+		List<Integer> factors = new LinkedList<>();
+
+		int max = Math.max(number, number / 2);
+		for (int i = 0; i <= max; i++) {
+
+			for (int j = i; j <= max; j++) {
+				if (i * j == number) {
+					factors.add(i);
+					if (i != j) {
+						factors.add(j);
+					}
+				}
+			}
+		}
+
+		factors.sort(Integer::compareTo);
+		return factors;
+	}
+}
---
+++ src/test/java/com/tealium/NumberMachineTest.java
@@ -0,0 +1,39 @@
+package com.tealium;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class NumberMachineTest {
+
+	NumberMachine machine;
+
+	@Before
+	public void setup() {
+		machine = new NumberMachine();
+	}
+
+	@Test
+	public void getFactors() {
+		 List<Integer> factors = machine.getFactors(0);
+		assertThat(factors, is(Arrays.asList(0)));
+
+		factors = machine.getFactors(1);
+		assertThat(factors, is(Arrays.asList(1)));
+
+		factors = machine.getFactors(2);
+		assertThat(factors, is(Arrays.asList(1, 2)));
+
+		factors = machine.getFactors(3);
+		assertThat(factors, is(Arrays.asList(1, 3)));
+
+		factors = machine.getFactors(4);
+		assertThat(factors, is(Arrays.asList(1, 2, 4)));
+
+	}
+}
